647536fb6176d5ecbaae2a5ad01e40d97e9866b9
Pornjed Sakgitjarung <ponnuki@gmail.com>
2017-03-27 12:50:13 +0700

    Move send message procedure into ok status

--- aqi-bot.php
+++ aqi-bot.php
@@ -59,31 +59,31 @@ if(isset($cityName) && !is_null($cityName)){
 							"SO2: ". $obj -> iaqi -> so2 -> v ."\n".
 							"Wind: ". $obj -> iaqi -> w -> v ."\n".
 							"Updated: ". $obj -> time -> s;
+							
+			//Send response text to target group id
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => $groupId,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 		}
-
-		//Send response text to target group id
-		$messages = [
-			'type' => 'text',
-			'text' => $text
-		];
-
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/push';
-		$data = [
-			'to' => $groupId,
-			'messages' => [$messages],
-		];
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
 	}catch(Exception $ex){
 		echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
